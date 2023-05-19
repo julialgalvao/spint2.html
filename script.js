32d1cafb2c094f59558d231cb60429e789ca95f8
julialgalvao <julialgalvao777@gmail.com>
2023-05-18 22:23:26 -0300

    site em andamento

--- script.js
+++ script.js
@@ -26,7 +26,45 @@ menuItems.forEach(item => {
     menuItems.forEach(item => {
       item.classList.remove('active');
     });
-    
     this.classList.add('active');
   });
 });
+
+
+//CONTATO, VALIDAÇAO 
+function validaEmail() {
+    var email = document.getElementById("email");
+    valueEmail = email.value;
+    //valida email
+    if (valueEmail.indexOf("@") == -1 || valueEmail.indexOf(".") == -1) {
+        email.classList.add("error");
+        return false;
+    } else {
+        email.classList.remove("error");
+        return true
+    }
+}
+
+function validaTelefone() {
+    var re = new RegExp('^\([0-9]{2}\)((3[0-9]{7})|(9[0-9]{8}))$');
+    var phone = document.getElementById("phone");
+    valueTelefone = phone.value;
+    console.log(valueTelefone)
+    console.log(re.test(valueTelefone))
+    return re.test(valueTelefone)
+}
+
+function validaForm() {
+
+    if (validaEmail() && validaTelefone()) {
+        alert("Formulário enviado com sucesso!");
+    } else {
+        alert("Formulário não enviado, verifique os campos");
+    }
+}
+
+//FAZER OS CARDS MUDAREM DE COR 
+function changeColor(element) {
+    var desiredColor = "#c9ecd4";
+    element.style.backgroundColor = desiredColor;
+  }
